92b8914be64d230da9a12aacea7cd9c4569d7561
luayida99 <e0543409@u.nus.edu>
2022-03-02 15:52:48 +0800

    do up to 1.3

--- progs/part1/lab3p1-shm.c
+++ progs/part1/lab3p1-shm.c
@@ -8,7 +8,22 @@
 
 int main() {
 
-    int i, j, pid;
+    int i, j, pid, shmid;
+    int* shm;
+
+    shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
+    if (shmid == -1) {
+        printf("Cannot create shared memory!\n");
+        exit(1);
+    }
+
+    shm = (int*)shmat(shmid, NULL, 0);
+    if (shm == (int*) -1) {
+        printf("Cannot attach to shared memory!\n");
+        exit(1);
+    }
+    
+    shm[0] = 0;
     
     for(i=0; i<NUM_PROCESSES; i++)
     {
@@ -18,6 +33,9 @@ int main() {
     }
 
     if(pid == 0) {
+
+        while (shm[0] != i);
+
         printf("I am child %d\n", i);
 
         for(j = i*10; j<i*10 + 10; j++){
@@ -27,11 +45,15 @@ int main() {
         }
 
         printf("\n\n");
+        shm[0]++;
     }
     else {
         for(i=0; i<NUM_PROCESSES; i++) 
             wait(NULL);
     }
+
+    shmdt((char*)shm);
+    shmctl( shmid, IPC_RMID, 0);
 
 }
 
