1646faf530e05cc17de32890365ccbafa610df5f
luayida99 <e0543409@u.nus.edu>
2022-03-02 23:50:57 +0800

    try 1.6 demo1, not working

--- progs/part1/lab3p1-sem.c
+++ progs/part1/lab3p1-sem.c
@@ -3,12 +3,20 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
+#include <semaphore.h>
 
 #define NUM_PROCESSES 5
 
 int main() {
 
-    int i, j, pid;
+    int i, j, pid, shmid;
+
+    shmid = shmget(IPC_PRIVATE, NUM_PROCESSES * sizeof(sem_t), IPC_CREAT | 0600);
+    sem_t* sems = (sem_t*) shmat(shmid, NULL, 0);
+
+    for (int k = 0; k < NUM_PROCESSES; k++) {
+        sem_init(&sems[k], 1, 0);
+    }
     
     for(i=0; i<NUM_PROCESSES; i++)
     {
@@ -18,6 +26,7 @@ int main() {
     }
 
     if(pid == 0) {
+        sem_post(&sems[i]);
         printf("I am child %d\n", i);
 
         for(j = i*10; j<i*10 + 10; j++){
@@ -27,11 +36,16 @@ int main() {
         }
 
         printf("\n\n");
+        
+        sem_wait(&sems[i]);
+        sem_destroy(&sems[i]);
     }
     else {
         for(i=0; i<NUM_PROCESSES; i++) 
             wait(NULL);
     }
+
+    shmctl(shmid, IPC_RMID, 0);
 
 }
 
