1875a0f16ae5f2fce50a64799457866d5aaecc22
luayida99 <e0543409@u.nus.edu>
2022-03-03 18:41:16 +0800

    partially do demo2, not working yet

--- progs/part2/barrier.c
+++ progs/part2/barrier.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+
+int nproc = 0;
+int count = 0;
+int shmid;
+sem_t *barrier;
 
 void init_barrier(int numproc) {
+    nproc = numproc;
+    count = 0;
+    shmid = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
+    barrier = (sem_t *) shmat(shmid, NULL, 0);
+    sem_init(barrier, 1, 0);
 }
 
 void reach_barrier() {
+    count = count + 1;
+    if (count == nproc) {
+        sem_post(barrier);
+    } else {
+        sem_wait(barrier);
+        sem_post(barrier);
+    }
 }
 
 void destroy_barrier(int my_pid) {
     if(my_pid != 0) {
-        // Destroy the semaphores and detach
-        // and free any shared memory. Notice
-        // that we explicity check that it is
-        // the parent doing it.
+        sem_destroy(barrier);
+        shmctl(shmid, IPC_RMID, 0);
     }
 }
 
