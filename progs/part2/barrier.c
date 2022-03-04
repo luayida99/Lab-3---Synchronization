5db29c0a0a377a32358697c2ff48eb69adc987ee
Benedict Chua Jun Jie <benedictchuajj@gmail.com>
2022-03-04 15:39:37 +0800

    Update barrier.c to work

--- progs/part2/barrier.c
+++ progs/part2/barrier.c
@@ -12,64 +12,46 @@ int* shm;
 int shmid_c;
 int shmid_n;
 int shmid_b;
-Barrier *barrier;
+sem_t* sems;
 
 void init_barrier(int numproc) {
     //shared memory for nproc and count
     shmid_c = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
-    if (shmid_c == -1) {
-        printf("Cannot create shared memory!\n");
-        exit(1);
-    }
-
     count = (int*) shmat(shmid_c, NULL, 0);
-    if (count == (int*) -1) {
-        printf("Cannot attach to shared memory!\n");
-        exit(1);
-    }
 
     shmid_n = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0600);
-    if (shmid_n == -1) {
-        printf("Cannot create shared memory!\n");
-        exit(1);
-    }
-
     nproc = (int*) shmat(shmid_n, NULL, 0);
-    if (nproc == (int*) -1) {
-        printf("Cannot attach to shared memory!\n");
-        exit(1);
-    }
 
     *nproc = numproc;
     *count = 0;
-    
-    printf("Count: %d", *count);
+    printf("Count: %d\n", *count);
+
     //shared memory for barrier
-    shmid_b = shmget(IPC_PRIVATE, sizeof(Barrier), IPC_CREAT | 0600);
-    barrier = (Barrier *) shmat(shmid_b, NULL, 0);
+    shmid_b = shmget(IPC_PRIVATE, 2 * sizeof(sem_t), IPC_CREAT | 0600);
+    sems = (sem_t*) shmat(shmid_b, NULL, 0);
 
     //sems[0] is barrier, sems[1] is mutex for count
-    sem_init(&barrier->sems[0], 1, 0);
-    sem_init(&barrier->sems[1], 1, 1);
+    sem_init(&sems[0], 1, 0);
+    sem_init(&sems[1], 1, 1);
 }
 
 void reach_barrier() {
-    printf("Reached barrier");
-    sem_wait(&barrier->sems[1]);
-    *count++;
-    printf("%d", *count);
-    sem_post(&barrier->sems[1]);
+    printf("Reached barrier\n");
+    sem_wait(&sems[1]);
+    *count = *count + 1;
+    printf("Current count %d\n", *count);
+    sem_post(&sems[1]);
     if (*count == *nproc) {
-        sem_post(&barrier->sems[0]);
+        sem_post(&sems[0]);
     } 
-    sem_wait(&barrier->sems[0]);
-    sem_post(&barrier->sems[0]);
+    sem_wait(&sems[0]);
+    sem_post(&sems[0]);
 }
 
 void destroy_barrier(int my_pid) {
     if(my_pid != 0) {
-        sem_destroy(&barrier->sems[0]);
-        sem_destroy(&barrier->sems[1]);
+        sem_destroy(&sems[0]);
+        sem_destroy(&sems[1]);
         shmctl(shmid_c, IPC_RMID, 0);
         shmctl(shmid_b, IPC_RMID, 0);
         shmctl(shmid_n, IPC_RMID, 0);
