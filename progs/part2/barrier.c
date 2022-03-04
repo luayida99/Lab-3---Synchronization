773cafe0d1ad747dde42469a742caa23ff823ae5
luayida99 <e0543409@u.nus.edu>
2022-03-04 12:08:06 +0800

    change demo2 structuring, not working

--- progs/part2/barrier.c
+++ progs/part2/barrier.c
@@ -4,33 +4,38 @@
 #include <semaphore.h>
 #include <sys/wait.h>
 #include <sys/shm.h>
+#include "barrier.h"
 
 int nproc = 0;
 int count = 0;
 int shmid;
-sem_t *barrier;
+Barrier *barrier;
 
 void init_barrier(int numproc) {
     nproc = numproc;
     count = 0;
-    shmid = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-    barrier = (sem_t *) shmat(shmid, NULL, 0);
-    sem_init(barrier, 1, 0);
+    shmid = shmget(IPC_PRIVATE, sizeof(Barrier), IPC_CREAT | 0600);
+    barrier = (Barrier *) shmat(shmid, NULL, 0);
+    //sems[0] is barrier, sems[1] is mutex for count
+    sem_init(&barrier->sems[0], 1, 0);
+    sem_init(&barrier->sems[1], 1, 1);
 }
 
 void reach_barrier() {
+    sem_wait(&barrier->sems[1]);
     count = count + 1;
+    sem_post(&barrier->sems[1]);
     if (count == nproc) {
-        sem_post(barrier);
-    } else {
-        sem_wait(barrier);
-        sem_post(barrier);
-    }
+        sem_post(&barrier->sems[0]);
+    } 
+    sem_wait(&barrier->sems[0]);
+    sem_post(&barrier->sems[0]);
 }
 
 void destroy_barrier(int my_pid) {
     if(my_pid != 0) {
-        sem_destroy(barrier);
+        sem_destroy(&barrier->sems[0]);
+        sem_destroy(&barrier->sems[1]);
         shmctl(shmid, IPC_RMID, 0);
     }
 }
--- progs/part2/barrier.h
+++ progs/part2/barrier.h
@@ -1,3 +1,9 @@
+#include <semaphore.h>
+
+typedef struct {
+    sem_t sems[2];
+} Barrier;
+
 void init_barrier(int);
 void reach_barrier();
 void destroy_barrier(int);
